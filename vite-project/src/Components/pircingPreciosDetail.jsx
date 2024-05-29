12fe26202138726d5a8d8775abfa92f801382559
rolo salom <roman.salom12@gmail.com>
2024-05-29 14:10:38 -0300

    arreglando trabl

--- vite-project/src/Components/pircingPreciosDetail.jsx
+++ vite-project/src/Components/pircingPreciosDetail.jsx
@@ -15,8 +15,10 @@ export const PrincingPriceDetail = () => {
                   Starter <span className="text-xs text-gray-400"></span>
                 </h3>
               </th>
-              <th className="p-4 text-align-center border whitespace-nowrap max-w-[150px]">
-                <h3 className="font-bold text-2xl">Professional</h3>
+              <th className="p-4 text-center border whitespace-nowrap max-w-[150px]">
+                <h3 className="font-bold text-2xl">
+                  Professional <span className="text-xs text-gray-400"></span>
+                </h3>
               </th>
               <th className="p-4 text-center border whitespace-nowrap max-w-[150px]">
                 <h3 className="font-bold text-2xl">
