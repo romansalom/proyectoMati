438774b02ee0358b21bf97de211936a03bab5e4e
rolo salom <roman.salom12@gmail.com>
2024-07-01 13:01:09 -0300

    moficiando server

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@emailjs/browser'],
+      external: ['aos'], // Indica a Vite que 'aos' es un módulo externo
     },
   },
 });
