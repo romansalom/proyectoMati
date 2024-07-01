d4f72f9bcca709f71a4ed303b2174c5352e3e1a0
rolo salom <roman.salom12@gmail.com>
2024-07-01 13:09:16 -0300

    modiifcando

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@emailjs/browser'], // Indica a Vite que este es un módulo externo
+    },
+  },
 });
